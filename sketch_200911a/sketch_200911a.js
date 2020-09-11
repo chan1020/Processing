1a4e4e1700cd260b9f1eb2e0eb3e5e478829e94f
chan1020 <70361816+chan1020@users.noreply.github.com>
2020-09-11 14:45:24 +0900

    11116 윤찬석 원5개&색깔

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500, 500);
+  
+  fill(255, 0, 255);
+  ellipse(100, 100, 100, 100);
+  fill(255, 255, 0);
+  ellipse(200, 100, 100, 100);
+  fill(0, 255, 0);
+  ellipse(100, 200, 100, 100);
+  fill(100, 255, 255);
+  ellipse(200, 200, 100, 100);
+  fill(0, 100, 255);
+  ellipse(300, 200, 100, 100);
+}
+
+
+function draw() {
+
+}
