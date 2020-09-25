8087e09aa9b038f7477d452272a5b99e500627b4
chan1020 <70361816+chan1020@users.noreply.github.com>
2020-09-25 15:12:23 +0900

    사각형 20개
    선생님 이거 여러개 해봤는데 20개를 어떻게 잘 맞출지를 모르겠어요.

---
+++ sketch_200925d/sketch_200925d.js
@@ -0,0 +1,13 @@
+function setup() {
+createCanvas(500, 500);
+noFill();
+for(let i=0; i<500; i+=int(random(0, 200))){
+for(let j=0; j<500; j+=int(random(0, 200))){
+rect(i, j, 100, 100);
+  }
+ }
+}
+
+
+function draw() {
+}
